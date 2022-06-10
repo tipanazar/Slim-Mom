f422a51868bc277ffbcc45d4fc7436622203d2fd
ololosh-tralevich <karpeko76@gmail.com>
2022-06-10 19:58:20 +0100

    гзв

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -1,5 +1,20 @@
-const Button = () => {
-    return <button>Button</button>
-}
+import PropTypes from "prop-types";
+
+const Button = ({ onClickBtn, btnText, type, className }) => {
+  // console.log('Button render')
+  return (
+    <button className={className} type={type} onClick={onClickBtn}>
+      {btnText}
+    </button>
+  );
+};
 
 export default Button;
+
+Button.propTypes = {
+  className: PropTypes.string.isRequired,
+  onClickBtn: PropTypes.func,
+  btnText: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  type: PropTypes.oneOf(["button", "submit", "reset"]).isRequired,
+};
