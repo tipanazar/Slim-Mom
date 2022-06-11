156c79690d261b8abad905885eec47294c7eccd0
Volodymyr Bendyk <92178622+wolodwolod@users.noreply.github.com>
2022-06-11 21:58:04 +0300

    Update Button.jsx

--- src/shared/components/Button/Button.jsx
+++ src/shared/components/Button/Button.jsx
@@ -1,13 +1,20 @@
-import React from 'react';
+import PropTypes from "prop-types";
 
-import styles from './Button.module.scss';
-
-const Button = ({ children, ...props }) => {
-   return (
-      <button className={styles.button} {...props}>
-         {children}
-      </button>
-   );
+const Button = ({ onClickBtn, btnText, type, className }) => {
+//   console.log('Button render')
+  return (
+    <button className={className} type={type} onClick={onClickBtn}>
+      {btnText}
+    </button>
+  );
 };
 
 export default Button;
+
+Button.propTypes = {
+  className: PropTypes.string.isRequired,
+  onClickBtn: PropTypes.func,
+  btnText: PropTypes.string.isRequired,
+  isActive: PropTypes.bool.isRequired,
+  type: PropTypes.oneOf(["button", "submit", "reset"]).isRequired,
+};
