0a82c8d5fcaca80bee76f7b7ada398ee9502f7bd
Olga Goncharenko <92627264+GoncharenkoO@users.noreply.github.com>
2022-06-12 23:32:53 +0300

    feat: add modal

--- src/modules/DailyCaloriesForm/DailyCaloriesForm.jsx
+++ src/modules/DailyCaloriesForm/DailyCaloriesForm.jsx
@@ -1,5 +1,8 @@
+import Modal from "../../shared/components/Modal/Modal";
+
 const DailyCaloriesForm = () => {
-  return <div>DailyCaloriesForm</div>;
+  // return <div>DailyCaloriesForm</div>;
+  return <Modal />;
 };
 
 export default DailyCaloriesForm;
