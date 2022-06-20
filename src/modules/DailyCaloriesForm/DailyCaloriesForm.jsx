f50d38f741f811d41b65571e602b42cf4fd269a7
Danyil Dykyi <91718159+moonshvdow@users.noreply.github.com>
2022-06-20 16:46:03 +0200

    done + calculatorPage but w/o globalStore

--- src/modules/DailyCaloriesForm/DailyCaloriesForm.jsx
+++ src/modules/DailyCaloriesForm/DailyCaloriesForm.jsx
@@ -1,44 +1,62 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import CalculatorСalorieForm from "../CalculatorСalorieForm";
 import Modal from "../../shared/components/Modal";
 import Button from "../../shared/components/Button";
 import SvgBtn from "./SvgComponents/SvgBtn";
 import SvgClose from "./SvgComponents/SvgClose";
-import register from "../CalculatorСalorieForm";
 import styles from "./dailyCaloriesForm.module.scss";
+
+import { getCaloriesAndProducts } from "../../shared/api/products"; 
+import Loader from "../../shared/components/Loader";
 
 const modalRoot = document.querySelector("#modalRoot");
 
-function DailyRate() {
-  let rand = Math.round(
-    10 * Number.parsefloat(register("currentWeight")) +
-      6.25 * Number.parsefloat(register("age")) -
-      5 * Number.parsefloat(register("height")) -
-      161 -
-      10 *
-        Number.parsefloat(register("currentWeight") - register("desiredWeight"))
-  );
-  return DailyRate(rand);
-}
+
 
 const DailyCaloriesForm = () => {
-  const [form, setForm] = useState("");
+  const [data, setData] = useState(null);
+  const [info, setInfo] = useState({
+    items: null,
+    loading: false,
+    error: null
+  })
 
   const [isModalOpen, setModalOpen] = useState(false); // Поставить, "true" что бы по умолчанию была открыта
+  
 
-  const onChange = (e) => {
-    e.preventDefault();
-    // setModalOpen(false);
-    // onSubmit(form);
+  useEffect(()=>{
+    if(!data){
+      return;
+    }
+    const fetchInfo = async () => {
+      try {
+        setInfo(prevData => ({
+          ...prevData,
+          loading: true,
+        }))
+        const result = await getCaloriesAndProducts(data, data.bloodType);
+        setInfo({
+          items: {...result},
+          loading: false,
+          error: null
+        });
+        setModalOpen(true);
+      } catch (err) {
+        setInfo({
+          items: null,
+          loading: false,
+          error: err
+        })
+      }
+    };
+    fetchInfo();
+  }, [data])
+  const onChange = (data) => {
+    setData(data);
   };
 
-  const handleChange = (ev) => {
-    const { name, value } = ev.target;
-    setForm((prevState) => {
-      return { ...prevState, [name]: value };
-    });
-  };
+
 
   isModalOpen
     ? (modalRoot.style.display = "flex")
@@ -48,20 +66,15 @@ const DailyCaloriesForm = () => {
     setModalOpen(false);
   };
 
+  const productsList = info.items?.products.map((product, idx)=> <li className={styles.productText} key={product._id}> {`${idx+1}.`} {product.title.ua}</li>)
+
   return (
     <>
       <CalculatorСalorieForm
         title={"Прорахуй свою добову норму калорій зараз"}
-        onSubmit={onChange}
-        handleChange={handleChange}
+        onChange={onChange}
       />
-      <Button
-        onClickBtn={() => setModalOpen(true)}
-        className={styles.modalOpenButton}
-        btnText={"Open Modal"}
-        type="button"
-      />
-      {!isModalOpen || (
+      {info.loading ? <Loader/> : !isModalOpen || (
         <Modal closeModal={closeModal}>
             <div className={styles.modalWindow}>
               <div className={styles.modalLogo}>
@@ -84,8 +97,8 @@ const DailyCaloriesForm = () => {
                     Ваша рекомендована добова норма калорій становить
                   </h2>
                   <p className={styles.modalCalory}>
-                    {DailyRate}
-                    <span className={styles.modalCalorySpan}>ккал</span>
+                      {info.items.calories}
+                    <span className={styles.modalCalorySpan}> ккал</span>
                   </p>
                 </div>
                 <div className={styles.products}>
@@ -93,11 +106,7 @@ const DailyCaloriesForm = () => {
                     Продукти, які вам не варто вживати
                   </p>
                   <ul className={styles.modalList}>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
+                    {productsList}
                   </ul>
                 </div>
               </div>
