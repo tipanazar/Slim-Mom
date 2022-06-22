b8db492dd78d6cd757c2975f1f1e3249ba8f0d14
MakarenkoYevhenii <92297867+MakarenkoYevhenii@users.noreply.github.com>
2022-06-22 23:01:42 +0300

    уйцу
    йуц

--- src/modules/Auth/Login/Login.jsx
+++ src/modules/Auth/Login/Login.jsx
@@ -56,10 +56,11 @@ const Login = () => {
   const InputLabelStyled = styled(InputLabel)`
     font-family: "Verdana";
     font-style: normal;
-    font-weight: 700;
+    font-weight: bold;
     font-size: 14px;
     line-height: 17px;
     letter-spacing: 0.04em;
+    color: #9B9FAA;
     :hover {
       color: #fc842d;
     }
@@ -99,7 +100,7 @@ const Login = () => {
     color: "#ffffff",
     marginTop: 60,
     marginRight: 32,
-      
+
     "&:hover": {
       backgroundColor: "#0069d9",
       borderColor: "#0062cc",
@@ -193,16 +194,16 @@ const Login = () => {
             </Alert>
           </Snackbar>
         )}
-   <form action="submit" onSubmit={submitForm} className={styles.form}>
-        <h1 className={styles.header}>ВХІД</h1>
-     
+        <form action="submit" onSubmit={submitForm} className={styles.form}>
+          <h1 className={styles.header}>ВХІД</h1>
+
           <div className={styles.inputWrapper}>
             <FormControl
               sx={{ m: 0, width: "29ch" }}
               variant="standard"
               color={validateEmail(userInfo.email) ? "warning" : "error"}
             >
-              <InputLabelStyled htmlFor="Login" className="InputLabel">
+              <InputLabelStyled htmlFor="Login" className="InputLabel" >
                 Email
               </InputLabelStyled>
               <Input
@@ -249,7 +250,9 @@ const Login = () => {
           </div>
 
           {userInfo.email.length > 5 && userInfo.password.length > 5 ? (
-             <div className={styles.buttonMobile}><ButtonColor type="submit">Логін</ButtonColor></div>
+            <div className={styles.buttonMobile}>
+              <ButtonColor type="submit">Логін</ButtonColor>
+            </div>
           ) : (
             <ButtonComp
               type="button"
